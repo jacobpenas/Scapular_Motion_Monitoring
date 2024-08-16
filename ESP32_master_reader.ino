40303e3405c2d7121efa2fd623e155364ac02053
jacobpenas <92338651+jacobpenas@users.noreply.github.com>
2024-08-16 12:03:53 -0400

    Add files via upload
    V1 of Arduino code used to establish I2C communication between ESP32 (master) and MSP430 (slave)

---
+++ ESP32_master_reader.ino
@@ -0,0 +1,32 @@
+#include "Wire.h"
+
+#define I2C_DEV_ADDR 0x02
+
+uint32_t i = 0;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Wire.begin();
+  Wire.setClock(100000);
+}
+
+void loop() {
+  delay(5000);
+
+  // Write message to the slave
+  Wire.beginTransmission(I2C_DEV_ADDR);
+  Wire.printf("Hello World! %lu", i++);
+  uint8_t error = Wire.endTransmission(true);
+  Serial.printf("endTransmission: %u\n", error);
+
+  // Read 6 bytes from the slave
+  uint8_t bytesReceived = Wire.requestFrom(I2C_DEV_ADDR, 6);
+  
+  Serial.printf("requestFrom: %u\n", bytesReceived);
+  if ((bool)bytesReceived) {  //If received more than zero bytes
+    uint8_t temp[bytesReceived];
+    Wire.readBytes(temp, bytesReceived);
+    log_print_buf(temp, bytesReceived);
+  }
+}
